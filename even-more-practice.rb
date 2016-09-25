3f29ce24b0208e976c82d73576a9a194147a3e91
betty <betty@casper.com>
2016-09-25 12:36:21 -0400

    asking user's name

--- even-more-practice.rb
+++ even-more-practice.rb
@@ -7,6 +7,14 @@
 #ask a user for their last name
 #store it in a variable called 'last_name'
 
+puts("What is your first name?")
+first_name = gets
+
+puts("What is your middle name?")
+middle_name = gets
+
+puts("What is your last name?")
+last_name = gets
 
 full_name = "#{first_name} + #{middle_name} + #{last_name}"
 
