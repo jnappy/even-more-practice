ad48111942b76708c9f66858fb81deece6103f9e
Jay <jpnappy@gmail.com>
2016-09-25 11:15:31 -0400

    first commit

---
+++ even-more-practice.rb
@@ -0,0 +1,13 @@
+#ask a user for their first name
+#store it in a variable called 'first_name'
+
+#ask a user for their middle name
+#store it in a variable called 'middle_name'
+
+#ask a user for their last name
+#store it in a variable called 'last_name'
+
+
+full_name = "#{first_name} + #{middle_name} + #{last_name}"
+
+puts("Your full name is #{full_name}")
