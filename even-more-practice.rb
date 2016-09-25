ea455d3c6e8538b10d7bc2b97b6d91f93118d490
Sean Manion <seanmanion@Seans-MacBook-Pro.local>
2016-09-25 12:42:34 -0400

    Updated capitalize, removed +

--- even-more-practice.rb
+++ even-more-practice.rb
@@ -7,7 +7,15 @@
 #ask a user for their last name
 #store it in a variable called 'last_name'
 
+puts "Hi, What's your first name?"
+first_name = gets.strip.downcase.capitalize
 
-full_name = "#{first_name} + #{middle_name} + #{last_name}"
+puts "What's your middle name?"
+middle_name= gets.strip.downcase.capitalize
+
+puts "what's your last name?"
+last_name = gets.strip.downcase.capitalize
+
+full_name = "#{first_name} #{middle_name} #{last_name}"
 
 puts("Your full name is #{full_name}")
